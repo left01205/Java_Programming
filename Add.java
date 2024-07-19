8c101025b1d218d2578319e75b39228327c75392
left01205 <student4.ai@csvtu.ac.in>
2024-07-19 21:17:53 +0530

    Adding File with simple arithematic addition and  output

---
+++ Add.java
@@ -0,0 +1,10 @@
+//package Java_Programming;
+
+public class Add {
+    public static void main(String[] args) {
+        System.out.println("Sum of 10 and 30 is :");
+        System.out.println(10 + 30);
+}
+
+    
+}
